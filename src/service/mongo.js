613703f90fdd6e8699519acad599406ac1258c38
João Ougano <pedroougano@gmail.com>
2023-05-12 01:15:06 -0300

    feat: add popupateDb function to mongoService

--- src/service/mongo.js
+++ src/service/mongo.js
@@ -9,4 +9,20 @@ const getDbCount = async () => {
   }
 };
 
-module.exports = { getDbCount };
+/**
+ * Receives a rate list and splits it into smallers rates
+ * @currencyList rates object
+ * @returns {array}
+ */
+
+const populateDb = async (currencyList) => {
+  try {
+    const insertedData = await Currency.create(currencyList);
+    console.log("Db successfully populated!");
+    return insertedData;
+  } catch (error) {
+    throw new Error(`Failed to populate db" ${error?.message}`);
+  }
+};
+
+module.exports = { getDbCount, populateDb };
