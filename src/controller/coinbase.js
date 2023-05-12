e905a5f5b46f3a161dfc5a75fc8d43f34bcef0c3
João Ougano <pedroougano@gmail.com>
2023-05-11 23:55:43 -0300

    feat: create coinbase controller

---
+++ src/controller/coinbase.js
@@ -0,0 +1,15 @@
+const coinbaseService = require("../service/coinbase");
+
+const getCurrencyRates = async (req, res) => {
+  try {
+    const rates = await coinbaseService.fetchCurrencyRates();
+    res.status(200).json(rates);
+  } catch (error) {
+    console.error(error);
+    res
+      .status(503)
+      .json({ error: true, message: "Failed to fetch currency rates" });
+  }
+};
+
+module.exports = { getCurrencyRates };
