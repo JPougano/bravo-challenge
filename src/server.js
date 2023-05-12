510dd405bbfdf81a9ef191ba2ec15a036ccf58b4
João Ougano <pedroougano@gmail.com>
2023-05-11 22:03:07 -0300

    feat: create basic route structure

--- src/server.js
+++ src/server.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const morgan = require("morgan");
+const router = require("./routes");
 const { SERVER_PORT } = process.env;
 
 const PORT = SERVER_PORT || 5001;
@@ -7,6 +8,7 @@ const PORT = SERVER_PORT || 5001;
 const app = express();
 
 app.use("/", morgan("dev"));
+app.use("/", router);
 
 app.listen(PORT, () => {
   console.log(`Server up and running on port ${PORT}`);
