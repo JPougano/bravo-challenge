0eff674fe2f8738385dac7dfbd5bb30227e9826c
João Ougano <pedroougano@gmail.com>
2023-05-12 00:31:41 -0300

    feat: stablish basic mongodb connection

--- src/server.js
+++ src/server.js
@@ -1,15 +1,35 @@
 const express = require("express");
+const mongoose = require("mongoose");
+const assert = require("assert");
 const morgan = require("morgan");
 const router = require("./routes");
-const { SERVER_PORT } = process.env;
-
-const PORT = SERVER_PORT || 5001;
+const { SERVER_PORT, DB_CONNECTION_URI } = process.env;
 
 const app = express();
+const PORT = SERVER_PORT || 5001;
 
-app.use("/", morgan("dev"));
-app.use("/", router);
+assert.ok(
+  DB_CONNECTION_URI,
+  "DB_CONNECTION_URI must be provided before using this application"
+);
 
-app.listen(PORT, () => {
-  console.log(`Server up and running on port ${PORT}`);
-});
+(async () => {
+  try {
+    await mongoose.connect(DB_CONNECTION_URI);
+    console.log("Successfully connected to mongoDb");
+
+    startServer();
+  } catch (error) {
+    console.error("Error setting up server:", error);
+    process.exit(1);
+  }
+})();
+
+const startServer = () => {
+  app.use("/", morgan("dev"));
+  app.use("/", router);
+
+  app.listen(PORT, () => {
+    console.log(`Server up and running on port ${PORT}`);
+  });
+};
