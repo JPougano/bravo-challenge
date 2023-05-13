5b67fb102a85973204e1c7467068398d189e1922
João Ougano <pedroougano@gmail.com>
2023-05-12 22:42:12 -0300

    feat: add currencyAddition endpoint

--- src/routes.js
+++ src/routes.js
@@ -1,7 +1,9 @@
 const express = require("express");
 const router = express.Router();
 const exchangeController = require("../src/controller/exchange");
+const mongoController = require("../src/controller/mongo");
 
 router.get("/currencyConversion", exchangeController.getConvertion);
+router.post("/currencyAddition", mongoController.createCurrency);
 
 module.exports = router;
