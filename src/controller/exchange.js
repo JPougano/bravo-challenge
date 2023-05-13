d62151734e320913e3bdc1b508e3b60870ef9249
João Ougano <pedroougano@gmail.com>
2023-05-13 12:45:57 -0300

    feat: add cache to currencyConversion workflow

--- src/controller/exchange.js
+++ src/controller/exchange.js
@@ -1,16 +1,10 @@
 const exchangeService = require("../service/exchange");
 const mongoService = require("../service/mongo");
+const redisClient = require("../../redis");
+const { CURRENCY_RATE_CACHE_KEY } = process.env;
 
 const getConvertion = async (req, res) => {
   const { from, to, amount } = req.query;
-
-  const dataToConvert = {
-    from: from.toUpperCase(),
-    to: to.toUpperCase(),
-    amount: amount,
-  };
-
-  let conversion;
 
   if (!from || !to || !amount) {
     res.status(503).json({
@@ -20,17 +14,54 @@ const getConvertion = async (req, res) => {
     });
     return;
   }
+
+  const dataToConvert = {
+    from: from.toUpperCase(),
+    to: to.toUpperCase(),
+    amount: amount,
+  };
+
+  await redisClient.connect();
+  const cachedData = await redisClient.get(CURRENCY_RATE_CACHE_KEY);
+  await redisClient.quit();
+
+  let conversion;
+
+  if (cachedData) {
+    const cachedRateList = JSON.parse(cachedData);
+    try {
+      conversion = exchangeService.convertCurrency(
+        cachedRateList,
+        dataToConvert
+      );
+      res.status(200).json({
+        from: dataToConvert.from,
+        to: dataToConvert.to,
+        amount: amount,
+        conversion: conversion,
+        fromCahe: true,
+      });
+    } catch (error) {
+      console.error(error);
+      res
+        .status(500)
+        .json({ error: true, message: "Failed to get currency conversion" });
+    }
+    return;
+  }
+
   try {
     const currecyRates = await mongoService.findCurrency([
       dataToConvert.from,
       dataToConvert.to,
     ]);
     conversion = exchangeService.convertCurrency(currecyRates, dataToConvert);
-    res.json({
+    res.status(200).json({
       from: dataToConvert.from,
       to: dataToConvert.to,
       amount: amount,
       conversion: conversion,
+      fromCahe: false,
     });
   } catch (error) {
     console.error(error);
