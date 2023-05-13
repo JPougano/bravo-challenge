e0fdd78c5446b74cd51d673018f7b93a8c622ed8
João Ougano <pedroougano@gmail.com>
2023-05-12 22:41:50 -0300

    feat: add createCurrency controller

--- src/controller/mongo.js
+++ src/controller/mongo.js
@@ -1,4 +1,5 @@
 const mongoService = require("../service/mongo");
+const joiValidation = require("../model/currencyJoi");
 
 const getCount = async (req, res) => {
   try {
@@ -10,4 +11,33 @@ const getCount = async (req, res) => {
   }
 };
 
-module.exports = { getCount };
+const createCurrency = async (req, res) => {
+  const { currency, rate } = req.body;
+
+  const newCurrency = {
+    currency: currency.toUpperCase(),
+    rate: rate,
+  };
+
+  const { error } = joiValidation.validate(newCurrency);
+  if (error) {
+    res.status(400).json(error.details);
+    throw new Error(error);
+  }
+
+  try {
+    const currencyExist = await mongoService.findCurrency(newCurrency.currency);
+    if (currencyExist.length != 0) {
+      res.status(500).json({ error: true, message: "Currency already exists" });
+      return;
+    }
+
+    const addedCurrency = await mongoService.addCurrency(newCurrency);
+    res.status(201).json(addedCurrency);
+  } catch (error) {
+    console.log(error);
+    res.status(404).json(error);
+  }
+};
+
+module.exports = { getCount, createCurrency };
