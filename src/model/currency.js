64c0453a2659a6847cd6ad0005d5e04d15d76f77
João Ougano <pedroougano@gmail.com>
2023-05-12 22:39:39 -0300

    refactor: change file name

--- src/model/currency.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require("mongoose");
-
-const Currency = mongoose.model("currency-rate", {
-  currency: { type: String, required: true },
-  rate: { type: String, required: true },
-  created_at: { type: Date, required: true, default: Date.now },
-});
-
-module.exports = Currency;
