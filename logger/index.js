175aab5132ed477e976f949cec60d2de60ce3b97
João Ougano <pedroougano@gmail.com>
2023-05-17 02:56:09 -0300

    feat: refactor logger and add mongodb as transports

--- logger/index.js
+++ logger/index.js
@@ -1,13 +1,55 @@
-const containerLog = require("./containerLog");
-const devLogger = require("./devLogger");
-const { NODE_ENV } = process.env;
+const { createLogger, format, transports } = require("winston");
+const { timestamp, combine, printf, colorize, errors, json } = format;
+const { LOG_LEVEL, NODE_ENV, DB_CONNECTION_URI_DEV_LOGGER } = process.env;
 
-let logger = null;
+const logger = () => {
+  const consoleFormat = printf(({ level, message, timestamp, stack }) => {
+    return `${level} [${timestamp}]: ${stack || message}`;
+  });
 
-if (NODE_ENV === "development") {
-  logger = devLogger();
-} else {
-  logger = containerLog();
-}
+  const mongodbFormat = printf(({ level, message, timestamp, stack }) => {
+    return `${level} [${timestamp}]: ${stack || message}`;
+  });
 
-module.exports = logger;
+  const transportArray = [
+    new transports.Console({
+      format: combine(
+        colorize(),
+        format.metadata(),
+        timestamp({ format: "YYYY-MM-DD HH:mm:ss" }),
+        errors({ stack: true }),
+        consoleFormat
+      ),
+    }),
+  ];
+
+  if (NODE_ENV === "development") {
+    transportArray.push(
+      new transports.MongoDB({
+        level: "warn",
+        db: DB_CONNECTION_URI_DEV_LOGGER,
+        collection: "bravo-logger",
+        format: combine(
+          timestamp(),
+          errors({ stack: true }),
+          format.uncolorize(),
+          format.metadata(),
+          json(),
+          mongodbFormat
+        ),
+        options: { useUnifiedTopology: true },
+        storeHost: true,
+      })
+    );
+  }
+
+  const logger = createLogger({
+    level: LOG_LEVEL,
+    defaultMeta: { service: "user-service" },
+    transports: transportArray,
+  });
+
+  return logger;
+};
+
+module.exports = logger();
